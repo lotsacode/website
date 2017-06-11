52e1a2d651bcc1ab3b8d8916b390bf8cbd10a143
vuoriov <vuoriov2@gmail.com>
2017-06-12 00:59:24 +0300

    updates

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ var session = require('express-session');
 var path = require('path');
 var fs = require('fs');
 var app = express();
-var jsons = [];
+var articles = [];
 app.set('view engine', 'ejs');
 
-function getDirectories(srcpath) {
-	return fs.readdirSync(srcpath).filter(function(file) {
-		return fs.statSync(path.join(srcpath, file)).isDirectory();
+// Logger
+function log(req) {
+    var date = new Date().toLocaleString();
+	var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+	var ip = req.connection.remoteAddress;
+	var line = date + " | " +  ip + " | " +  url;
+	fs.appendFile('log.txt', line + '\n', function (err) {
+		if (err) throw err;
 	});
+    console.log(line);
 }
-var folders = getDirectories("views");
+
+// Set up articles to memory
+var folders = fs.readdirSync("views").filter(function(file) {
+	return fs.statSync(path.join("views", file)).isDirectory();
+});
 folders.forEach(function(folder) {
 	var json = JSON.parse(fs.readFileSync("views/" + folder + '/meta.json', 'utf8'));
 	json.route = folder;
-	jsons.push(json);
+	articles.push(json);
 });
-jsons = jsons.sort(function(json1, json2) { return json2.dateInt - json1.dateInt });
+articles = articles.sort(function(json1, json2) { return json2.dateInt - json1.dateInt });
 Array.prototype.contains = function(obj) {
     var i = this.length;
     while (i--) {
@@ -28,39 +38,38 @@ Array.prototype.contains = function(obj) {
     return false;
 }
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-jsons = jsons.filter(function(x) {
-	return config.released.contains(x.route);
+articles = articles.filter(function(x) {
+	return !config.excluded.contains(x.route);
 });
 
-var request = require('request');
-var cheerio = require('cheerio');
-
-// GET requests
+// Get index
 app.get('/', function(req, res) {
-	console.log("Request from " + req.ip)
-	res.render("index.ejs", {lastPub: jsons[0].date, articles: jsons});
+	log(req);
+	res.render("index.ejs", {lastPub: articles[0].date, articles: articles});
 });
 
-app.get('/post/*', function(req, res) {
+// Get article
+app.get('/article/*', function(req, res) {
+	log(req);
 	try {
 		var meta = require('./views/' + req.params[0] + '/meta.json');
-		var index = jsons.indexOf(jsons.filter(function(article) {
+		var index = articles.indexOf(articles.filter(function(article) {
 			return article.title == meta.title;
 		})[0]);
-		var previousIndex = (index + 1) % jsons.length;
-		var previous = jsons[previousIndex];
+		var previousIndex = (index + 1) % articles.length;
+		var previous = articles[previousIndex];
 		var nextIndex = index - 1;
-		if (nextIndex < 0) nextIndex += jsons.length;
-		nextIndex = nextIndex % jsons.length;
-		var next = jsons[nextIndex];
-		res.render('paper.ejs', {lastPub: jsons[0].date, previous: previous, next: next, date: meta.date, title: meta.title, fileName: req.params[0] + "/view.ejs"});
+		if (nextIndex < 0) nextIndex += articles.length;
+		nextIndex = nextIndex % articles.length;
+		var next = articles[nextIndex];
+		res.render('article.ejs', {lastPub: articles[0].date, previous: previous, next: next, date: meta.date, title: meta.title, fileName: req.params[0] + "/view.ejs"});
 	} catch (e) { // Not found
 		console.log(e);
-	 	res.render("404.ejs", {lastPub: jsons[0].date});
+	 	res.render("404.ejs", {lastPub: articles[0].date});
 	}
 });
 
-// Files
+// Serve static files
 app.use('/static', express.static(__dirname + '/static'));
 
 // Start the server
--- views/how-to-build-a-web-scraper-with-nodejs/scraper.scala
+++
@@ -1,7 +0,0 @@
-object Scraper extends App {
-	def get(url: String) = {
-		println("Hello?")
-	}
-	get("http://www.google.com")
-}
-
